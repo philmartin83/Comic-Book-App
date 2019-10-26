f6acec573581a64da2479074b356a295c8341d31
Phil Martin <phil.martin1983@gmail.com>
2019-10-26 21:52:22 +0100

    Remove .DS_Store from everywhere

---
+++ ComicApp/Core/Constants/PrivateConstants.swift
@@ -0,0 +1,12 @@
+//
+//  PrivateConstants.swift
+//  ComicApp
+//
+//  Created by Philip Martin on 29/09/2019.
+//  Copyright © 2019 Phil Martin. All rights reserved.
+//
+
+import Foundation
+
+let privateKey = "" //"developer.marvel.com private key"
+let publicKey = "" //"developer.marvel.com public key"
