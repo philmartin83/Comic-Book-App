df66627f6d5ff37b9aa3fd30cfa5f2a64e9dc779
Phil Martin <phil.martin1983@gmail.com>
2019-11-19 22:37:08 +0000

    Clean up of request handler

--- ComicApp/Core/Networking/RequestHandler.swift
+++ ComicApp/Core/Networking/RequestHandler.swift
@@ -1,49 +1,44 @@
 //
 //  RequestHandler.swift
-//  ComicApp
+//  ComicBookApp
 //
-//  Created by Phil Martin on 10/09/2019.
-//  Copyright © 2019 Phil Martin. All rights reserved.
+//  Created by Philip Martin on 30/10/2019.
+//  Copyright © 2019 Philip Martin. All rights reserved.
 //
 
 import Foundation
 
 class RequestHandler{
-    let baseURL = "http://gateway.marvel.com"
-    let timeout: TimeInterval = 10
-    let httpGET = "GET"
-    let httpPOST = "POST"
+    private let baseURL = "https://gateway.marvel.com"
     
     func getCharacters() -> URLRequest{
         let endpoint = "/v1/public/characters"
-        let timestamp = Date().timeIntervalSince1970
-        let url = baseURL + endpoint + "?" + queryStringForURL(timestamp: timestamp, addLimit: true)
-        var request = URLRequest(url: URL(string: url)!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: timeout)
-        request.httpMethod = httpGET
-        print(request)
-        return request
-    }
-    
-    func getComics(comicSeries: String) -> URLRequest{
-        let timestamp = Date().timeIntervalSince1970
-        let url = comicSeries + "?" + queryStringForURL(timestamp: timestamp, addLimit: false)
-        var request = URLRequest(url: URL(string: url)!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: timeout)
-        request.httpMethod = httpGET
+        let url = baseURL + endpoint + buildQueryString()
+        
+        let request = URLRequest(url: URL(string: url)!)
         return request
         
     }
     
-    private func buildAuthTokenForRequest(timeStamp: TimeInterval) -> String{
-        // (ts+private+public)
-        let hashThisString = "\(timeStamp)" + privateKey + publicKey
-        return MD5(str: hashThisString)
+    func getComics(id: Int) -> URLRequest{
+        // https://gateway.marvel.com/v1/public/characters/1011334/comics
+        let endpoint = "/v1/public/characters/\(id)/comics"
+        let url = baseURL + endpoint + buildQueryString()
+        
+        let request = URLRequest(url: URL(string: url)!)
+        return request
+        
     }
     
-    private func queryStringForURL(timestamp: TimeInterval, addLimit: Bool) -> String{
-        var queryString = "ts=\(timestamp)&apikey=\(publicKey)&hash=" + buildAuthTokenForRequest(timeStamp: timestamp)
-        if addLimit{
-            queryString = queryString + "&limit=70"
-        }
+    private func buildQueryString() -> String{
+        let timeStamp = Date().timeIntervalSince1970
+        
+        let queryString = "?ts=\(timeStamp)&apikey=\(publicKey)&hash=\(buildHashToken(timestamp: timeStamp))"
         return queryString
+    }
+    
+    private func buildHashToken(timestamp: TimeInterval) -> String{
+        let unhashedString = "\(timestamp)" + privateKey + publicKey
+        return MD5(str: unhashedString)
     }
 }
